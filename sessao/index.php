4350052d965da2e9e2540f3c5747b0001311a8f0
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2023-01-04 17:22:00 -0300

    super global $_SESSION em php, manter e destruir sessão

---
+++ sessao/index.php
@@ -0,0 +1,22 @@
+<?php
+    //inicia sessao
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $_SESSION["favcolor"] = "green";
+        $_SESSION["favanimal"] = "cat";
+        echo "As variáveis de sessão estão definidas";
+
+        print_r($_SESSION);
+    ?>
+</body>
+</html>
