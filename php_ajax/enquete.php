2a0c7053a48164a91383ccf7d27077ebab6f44a8
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2023-05-09 15:04:02 -0300

    Ajax com php finally

--- php_ajax/enquete.php
+++ php_ajax/enquete.php
@@ -1,30 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
-        function getVote(int){
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState==4 && this.status==200){
-                    document.getElementById("poll").innerHTML = this.responseText;
-                }
-                xmlhttp.opne("GET", "poll_vote.php?vote="+int, true);;
-                xmlhttp.send();
-            }
-        }
-    </script>
+<script>
+function getVote(int) {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+  xmlhttp.send();
+}
+</script>
 </head>
 <body>
+
     <div id="poll">
-        <h3>Você gosta de PHP e AJAX?</h3>
-        <form>
-            Sim: <input type="radio" name="vote" id="isim"  value="0" onclick="getVote(this.value)"><br>
-            Não: <input type="radio" name="vote" id="inao" value="1" onclick="getVote(this.value)">
-        </form>
+    <h3>Do you like PHP and AJAX so far?</h3>
+    <form>
+    Yes: <input type="radio" name="vote" value="0" onclick="getVote(this.value)"><br>
+    No: <input type="radio" name="vote" value="1" onclick="getVote(this.value)">
+    </form>
     </div>
+
 </body>
 </html>
